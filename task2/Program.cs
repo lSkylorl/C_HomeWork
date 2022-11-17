d1460cdf58801567fea607e2c65922e8d0fc7171
Maxim Alexandrovskiy <hi136maksim11@mail.ru>
2022-11-18 03:50:44 +0400

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA > numberB)
+{
+    Console.WriteLine($"max {numberA}");
+    Console.WriteLine($"min {numberB}");
+}
+else
+{
+    Console.WriteLine($"max {numberB}");
+    Console.WriteLine($"min {numberA}");
+}
