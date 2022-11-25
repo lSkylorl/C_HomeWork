271ac50162086d9a97365e7d7591467beaa5129e
Maxim Alexandrovskiy <hi136maksim11@mail.ru>
2022-11-25 09:00:48 +0400

    add task21 and task19

---
+++ task19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.Write($"{number} - Палиндром");
+    }
+    else
+    {
+        Console.Write($"{number} - Не палиндром");
+    }
+}
+else
+{
+    Console.Write($"{number} - не является пятизначным");
+}
---
+++ task21/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите координату X точки A: ");
+int xA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y точки A: ");
+int yA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Z точки A: ");
+int zA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату X вточки B: ");
+int xB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y вточки B: ");
+int yB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Z вточки B: ");
+int zB = Convert.ToInt32(Console.ReadLine());
+
+double len = Math.Sqrt((xB - xA) * (xB - xA) + (yB - yA) * (yB - yA) + (zA - zB) * (zA - zB));
+Console.WriteLine($"Длинна отрезка: {len}");
