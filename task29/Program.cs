0d696bec0a9b33feb6b9ddac06e544eaee0c3122
Maxim Alexandrovskiy <hi136maksim11@mail.ru>
2022-11-29 03:40:26 +0400

    add task25/27/29

---
+++ task29/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите длинну массива: ");
+int lenArr = Convert.ToInt32(Console.ReadLine());
+
+int[] Arr = new int[lenArr];
+for (int i = 0; i < Arr.Length; i++)
+{
+    Arr[i] = new Random().Next(0,9);
+    Console.Write($"{Arr[i]} ");
+}
