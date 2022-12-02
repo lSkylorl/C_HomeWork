8d4153f283a4dab9f239df484ae6aea7e97ac7c9
Maxim Alexandrovskiy <hi136maksim11@mail.ru>
2022-12-02 04:43:37 +0400

    add task34/35/36/38

---
+++ task34/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] num = new int[size];
+int count = 0;
+
+Console.Write("массив: ");
+Arr(num);
+void Arr(int[] num)
+{
+    for(int i = 0; i < size; i++)
+    {
+        num[i] = new Random().Next(100,1000);
+        Console.Write($"{num[i]} ");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < size; i++)
+{
+    if (num[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"массив из {size} чисел, из которых {count} четные");
