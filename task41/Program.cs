8934c1bcd06aab19dba8fe6e4c01c7bee883d35d
Maxim Alexandrovskiy <hi136maksim11@mail.ru>
2022-12-06 02:32:31 +0400

    add task41/43

---
+++ task41/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите элементы массива: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Количество элементов > 0: {count}");
